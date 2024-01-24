af58cab9bdd74ab1d0ca110503039d0fc94be60a
SahilShameerDev <smsahilms346@gmail.com>
2024-01-24 19:50:20 +0530

    Hero and Logo Issue Fixed

--- app/page.tsx
+++ app/page.tsx
@@ -4,20 +4,13 @@ import NavBar from "@/components/NavBar/NavBar";
 import Gdsc from "@/components/GDSC/Gdsc";
 
 const imageStyle = {
-  zIndex: -1,
+  zIndex: -2,
 };
 
 export default function Home() {
   return (
     <div>
-      <Image
-        src="/hero.png"
-        alt=""
-        layout="fill"
-        objectFit="cover"
-        objectPosition="top"
-        style={imageStyle}
-      />
+      <img src="/hero.png" alt="" className={style.hero} />
       <NavBar />
       <Gdsc/>
     </div>
--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import Image from "next/image";
+import style from './NavBar.module.css'
 import SearchBar from "../SearchBar/SearchBar";
 import FilterButton from "../FilterButton/FilterButton";
 
 const NavBar = () => {
   return (
     <div className="navBar">
-      <div className="logo">
-        <Image src="/gdsc_logo.png" alt="logo" width={120} height={120} />
+      <div >
+        <Image src="/gdsc_logo.png" alt="logo" width={120} height={120} className={style.logo} />
       </div>
       <SearchBar />
       <FilterButton />
