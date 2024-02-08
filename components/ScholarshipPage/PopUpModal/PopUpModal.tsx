29b6327f7b72fd29d8a3fd9c000711fc5f6a7270
liyashaji <95729024+liyashaji@users.noreply.github.com>
2024-02-08 22:18:26 +0530

    pop up card styling

--- components/ScholarshipPage/PopUpModal/PopUpModal.tsx
+++ components/ScholarshipPage/PopUpModal/PopUpModal.tsx
@@ -6,9 +6,9 @@ import Image from "next/image";
 const PopUpModal = () => {
   return (
     <div>
-      <h1 className={styles.heading} >JN Tata Endowment Loan Scholarship 2024-25</h1>
-      <div className={styles.deadline}><p><Image src='/calender.png' alt="" height={20} width={20}/>Deadline15-Mar-2024</p></div>
-      <h2>Eligiblity</h2>
+      <h1 className={styles.headingmain} >JN Tata Endowment Loan Scholarship 2024-25</h1>
+      <div className={styles.deadline}><p><Image className={styles.image} src='/calender.png' alt="" height={20} width={20}/>Deadline15-Mar-2024</p></div>
+      <h2 className={styles.heading}>Eligiblity</h2>
       <ul className={styles.eligible}>
         <li>
           Candidates should be Indians, not older than 45 years as of June 30,
@@ -34,15 +34,16 @@ const PopUpModal = () => {
           scholarship award, typically in July of any calendar year.
         </li>
       </ul>
-      <h2>Benefits</h2>
-      <p>Loan scholarship of up to INR 10 lakh</p>
-      <h2>Documents</h2>
-      <p>
+      <h2  className={styles.heading}>Benefits</h2>
+      <div className={styles.eligible}><p>Loan scholarship of up to INR 10 lakh</p> </div>
+      <h2  className={styles.heading}>Documents</h2>
+      <div className={styles.eligible}> <p>
         The entire scholarship program comprises four phases and candidates will
         be informed about the status of their application at the end of each
         phase.
-      </p>
-      <h2>Contact</h2>
+      </p></div>
+      <h2  className={styles.heading}>Contact</h2>
+      <div className={styles.eligible}>
       <p>
       <Image src='/phone.png' alt="" height={20} width={20}/>
       00000 11111
@@ -51,7 +52,11 @@ const PopUpModal = () => {
       <Image src='/email.png' alt="" height={20} width={20}/>
         name@mail
         </p>
+      </div>
+      
+      <div className={styles.applybutton}>
       <button>Apply Now</button>
+      </div>
     </div>
   );
 };
