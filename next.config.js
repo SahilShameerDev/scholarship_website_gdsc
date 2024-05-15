c250e3d832eaf57fc54d1a77d53f32b64e763777
SahilShameerDev <smsahilms346@gmail.com>
2024-05-15 13:38:20 +0530

    Publishing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    basePath: "/scholarship_website_gdsc",
+    output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+}
 
 module.exports = nextConfig
